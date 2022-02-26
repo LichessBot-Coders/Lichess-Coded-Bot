1267d2ced94624c3e656ebc8e10d836a47d4b459
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-02-26 07:26:43 +0530

    Update conversation.py

--- conversation.py
+++ conversation.py
@@ -15,28 +15,26 @@ class Conversation:
     command_prefix = "!"
 
     def react(self, line, game):
-        logger.info("*** {} [{}] {}: {}".format(self.game.url(), line.room, line.username, line.text.encode("utf-8")))
-        if (line.text[0] == self.command_prefix):
+        logger.info(f'*** {self.game.url()} [{line.room}] {line.username}: {line.text.encode("utf-8")}')
+        if line.text[0] == self.command_prefix:
             self.command(line, game, line.text[1:].lower())
 
     def command(self, line, game, cmd):
         if cmd == "commands" or cmd == "help":
-            self.send_reply(line, "Supported commands: !wait, !name, !howto, !eval, !queue, !time")
+            self.send_reply(line, "Supported commands: !wait (wait a minute for my first move), !name, !howto, !eval, !queue, !time")
         elif cmd == "wait" and game.is_abortable():
-            game.ping(60, 120)
+            game.ping(60, 120, 120)
             self.send_reply(line, "Waiting 60 seconds...")
         elif cmd == "name":
             name = game.me.name
-            self.send_reply(line, "{} using c++ and java codes running {} (lichess-bot v{}) on heroku server.".format(name, self.engine.name(), self.version))
-        elif cmd == "id":
-            self.send_reply(line, "RaviharaV")
+            self.send_reply(line, f"{name} running {self.engine.name()} (lichess-bot v{self.version})")
         elif cmd == "howto":
-            self.send_reply(line, "How to run your own bot: Check out 'Lichess Bot API' or go to https://github.com/LichessBot-Coders/Lichess-Coded-Bot")
+            self.send_reply(line, "How to run: Check out 'Lichess Bot API'")
         elif cmd == "eval" and line.room == "spectator":
             stats = self.engine.get_stats(for_chat=True)
             self.send_reply(line, ", ".join(stats))
         elif cmd == "eval":
-            self.send_reply(line, "I will defeat you in 5 moves. just kidding it is a command only for spectators.")        
+            self.send_reply(line, "I will defeat you in 5 moves. just kidding it is a command only for spectators")
         elif cmd == "queue":
             if self.challengers:
                 challengers = ", ".join([f"@{challenger.challenger_name}" for challenger in reversed(self.challengers)])
@@ -47,9 +45,15 @@ class Conversation:
             now = datetime.now()
             current_time = now.strftime("%H:%M:%S")
             self.send_reply(line, "current time is {}".format(current_time))
+        elif cmd == "id":
+            self.send_reply(line, "RaviharaV")
 
     def send_reply(self, line, reply):
         self.xhr.chat(self.game.id, line.room, reply)
+
+    def send_message(self, room, message):
+        if message:
+            self.send_reply(ChatLine({"room": room}), message)
 
 
 class ChatLine:
